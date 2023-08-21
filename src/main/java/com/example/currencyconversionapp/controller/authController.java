5995a8fba65d11dddfb065394b3e30325a3f8fbc
amr mahmoud <amrmahmoud1900@gmail.com>
2023-08-22 01:46:07 +0300

    hello from pipline

--- src/main/java/com/example/currencyconversionapp/controller/authController.java
+++ src/main/java/com/example/currencyconversionapp/controller/authController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class authController {
     @GetMapping
     public String get(){
-        return "Hello world";
+        return "Hello amr";
     }
 }
