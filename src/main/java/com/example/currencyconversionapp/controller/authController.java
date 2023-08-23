69c498ac4ee8e37754c5563c5c268794337a42e4
amr mahmoud <amrmahmoud1900@gmail.com>
2023-08-23 13:54:52 +0300

    .

--- src/main/java/com/example/currencyconversionapp/controller/authController.java
+++ src/main/java/com/example/currencyconversionapp/controller/authController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1")
 public class authController {
+
     @GetMapping
     public String get(){
         return "Hello amr";
